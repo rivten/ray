ffd59995a4b0da4dfe36fe801e7bbd14e664152d
rivten <rivten.grey@gmail.com>
2018-05-14 22:29:47 +0200

    bitmap display

---
+++ code/build.sh
@@ -0,0 +1,31 @@
+CODE_PATH="$(dirname "$0")"
+CTIME_EXEC="$CODE_PATH/../../ctime/ctime"
+CTIME_TIMING_FILE="$CODE_PATH/../build/compile_ray.ctm"
+
+if [ ! -f "$CTIME_EXEC" ]; then
+	cc -O2 -Wno-unused-result "$CODE_PATH/../../ctime/ctime.c" -o "$CTIME_EXEC"
+fi
+
+$CTIME_EXEC -begin "$CTIME_TIMING_FILE"
+
+CommonFlags="-g -std=c++11 -Werror -Wall -Wextra -Wcast-align -Wmissing-noreturn -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wmissing-include-dirs -Wno-old-style-cast -Woverloaded-virtual -Wredundant-decls -Wsign-promo -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option -Wno-write-strings -Wno-absolute-value -Wno-cast-align -Wno-unused-parameter -lm"
+
+if [ -n "$(command -v clang++)" ]
+then
+	CXX=clang++
+	CommonFlags+=" -Wno-missing-braces -Wno-null-dereference -Wno-self-assign"
+else
+	CXX=c++
+	CommonFlags+=" -Wno-unused-but-set-variable"
+fi
+
+CommonLinkerFlags="-l SDL2"
+
+mkdir -p "$CODE_PATH/../build"
+pushd "$CODE_PATH/../build"
+
+$CXX $CommonFlags ../code/ray.cpp $CommonLinkerFlags -o ray-x86_64
+
+popd
+
+$CTIME_EXEC -end "$CTIME_TIMING_FILE"
---
+++ code/ray.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <SDL.h>
+#else
+#include <SDL2/SDL.h>
+#endif
+
+#include "rivten.h"
+#include "rivten_math.h"
+
+#define SDL_CHECK(Op) {s32 Result = (Op); Assert(Result == 0);}
+
+global_variable u32 GlobalWindowWidth = 512;
+global_variable u32 GlobalWindowHeight = 512;
+global_variable bool GlobalRunning = true;
+
+internal u32
+RGBToPixel(u8 R, u8 G, u8 B)
+{
+	u8 A = 0xFF;
+	u32 Result = (A << 24) |
+			     (R << 16) |
+			     (G << 8)  |
+			     (B << 0);
+
+	return(Result);
+}
+
+internal void
+DrawBackbuffer(u32* Backbuffer)
+{
+	for(u32 Y = 0; Y < GlobalWindowHeight; ++Y)
+	{
+		for(u32 X = 0; X < GlobalWindowWidth; ++X)
+		{
+			u32* Pixel = Backbuffer + X + Y * GlobalWindowWidth;
+			u8 R = (u32)(255.0f * (float(X) / float(GlobalWindowWidth))) & 0xFF;
+			u8 G = (u32)(255.0f * (float(Y) / float(GlobalWindowHeight))) & 0xFF;
+			u8 B = 40;
+			*Pixel = RGBToPixel(R, G, B);
+		}
+	}
+}
+
+int main(int ArgumentCount, char** Arguments)
+{
+	u32 SDLInitParams = SDL_INIT_EVERYTHING;
+	SDL_CHECK(SDL_Init(SDLInitParams));
+
+
+	u32 WindowFlags = SDL_WINDOW_SHOWN;
+	SDL_Window* Window = SDL_CreateWindow("PathTracer", 
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			GlobalWindowWidth, GlobalWindowHeight,
+			WindowFlags);
+
+	Assert(Window);
+
+	SDL_Surface* Screen = SDL_GetWindowSurface(Window);
+	Assert(Screen);
+
+	u32* Backbuffer = AllocateArray(u32, GlobalWindowWidth * GlobalWindowHeight);
+
+	while(GlobalRunning)
+	{
+		// NOTE(hugo): Input
+		// {
+		SDL_Event Event = {};
+		while(SDL_PollEvent(&Event))
+		{
+			switch(Event.type)
+			{
+				case SDL_QUIT:
+					{
+						GlobalRunning = false;
+					} break;
+				default:
+					{
+					} break;
+			}
+		}
+		// }
+
+
+		DrawBackbuffer(Backbuffer);
+		memcpy(Screen->pixels, Backbuffer, sizeof(u32) * GlobalWindowWidth * GlobalWindowHeight);
+		SDL_UpdateWindowSurface(Window);
+	}
+
+	SDL_DestroyWindow(Window);
+	SDL_Quit();
+	return(0);
+}
