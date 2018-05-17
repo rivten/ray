3daf7375caca40198421b1755f4eca1591ab1937
rivten <rivten.grey@gmail.com>
2018-05-17 14:04:25 +0200

    effectively emitting rays

--- code/ray.cpp
+++ code/ray.cpp
@@ -34,9 +34,27 @@ struct camera
 	v3 ZAxis;
 };
 
+struct sphere
+{
+	float Radius;
+	v3 P;
+};
+
 struct render_state
 {
+	u32 SphereCount;
+	sphere Spheres[256];
+
 	camera Camera;
+	float FocalLength;
+	float FoV;
+	float AspectRatio;
+};
+
+struct ray
+{
+	v3 Start;
+	v3 Dir;
 };
 
 internal void
@@ -47,10 +65,27 @@ DrawBackbuffer(render_state* RenderState, u32* Backbuffer)
 		for(u32 X = 0; X < GlobalWindowWidth; ++X)
 		{
 			u32* Pixel = Backbuffer + X + Y * GlobalWindowWidth;
-#if 1
+
+			// TODO(hugo): Most of this can be done outside the loop
+			ray Ray = {};
+			Ray.Start = RenderState->Camera.P;
+			v2 PixelRelativeCoordInScreen = V2((float(X) / float(GlobalWindowWidth)) - 0.5f, (float(Y) / float(GlobalWindowHeight)) - 0.5f);
+			float ScreenWidth = 2.0f * RenderState->FocalLength * Tan(0.5f * RenderState->FoV);
+			float ScreenHeight = ScreenWidth * RenderState->AspectRatio;
+			v3 CameraYAxis = Cross(RenderState->Camera.ZAxis, RenderState->Camera.XAxis);
+			v3 PixelWorldSpace = RenderState->Camera.P - RenderState->FocalLength * RenderState->Camera.ZAxis + 
+				PixelRelativeCoordInScreen.x * ScreenWidth * RenderState->Camera.XAxis + PixelRelativeCoordInScreen.y * ScreenHeight * CameraYAxis;
+			// TODO(hugo): Do we need to have a normalized direction ? Maybe not...
+			Ray.Dir = Normalized(PixelWorldSpace - Ray.Start);
+
+#if 0
 			u8 R = (u32)(255.0f * (float(X) / float(GlobalWindowWidth))) & 0xFF;
 			u8 G = (u32)(255.0f * (float(Y) / float(GlobalWindowHeight))) & 0xFF;
 			u8 B = 40;
+#else
+			u8 R = 0;
+			u8 G = 0;
+			u8 B = (u32)(255.0f * (0.5f * (- Ray.Dir.y) + 0.5f)) & 0xFF;
 #endif
 			*Pixel = RGBToPixel(R, G, B);
 		}
@@ -81,6 +116,9 @@ int main(int ArgumentCount, char** Arguments)
 	RenderState.Camera.P = V3(0.0f, 0.0f, 10.0f);
 	RenderState.Camera.XAxis = V3(1.0f, 0.0f, 0.0f);
 	RenderState.Camera.ZAxis = V3(0.0f, 0.0f, 1.0f);
+	RenderState.FoV = Radians(75.0f);
+	RenderState.FocalLength = 2.0f;
+	RenderState.AspectRatio = float(GlobalWindowHeight) / float(GlobalWindowWidth);
 
 	while(GlobalRunning)
 	{
