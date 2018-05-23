75b6632c4436fdddfac7f26617c58cccb9bd9bb6
rivten <rivten.grey@gmail.com>
2018-05-23 14:00:54 +0200

    see change in realtime + gamma correction + material system

--- code/build.bat
+++ code/build.bat
@@ -10,7 +10,7 @@ rem set SDLNetPath=C:\SDL2_net-2.0.1\
 rem set SDLNetBinPath=%SDLNetPath%\lib\x64\
 
 set UntreatedWarnings=/wd4100 /wd4244 /wd4201 /wd4127 /wd4505 /wd4456 /wd4996 /wd4003 /wd4706
-set CommonCompilerDebugFlags=/MT /Od /Oi /fp:fast /fp:except- /Zo /Gm- /GR- /EHa /WX /W4 %UntreatedWarnings% /Z7 /nologo /I %SDLPath%\include\ /I %STBPath% /I %RivtenPath%
+set CommonCompilerDebugFlags=/MT /O2 /Oi /fp:fast /fp:except- /Zo /Gm- /GR- /EHa /WX /W4 %UntreatedWarnings% /Z7 /nologo /I %SDLPath%\include\ /I %STBPath% /I %RivtenPath%
 set CommonLinkerDebugFlags=/incremental:no /opt:ref /subsystem:console %SDLBinPath%\SDL2.lib %SDLBinPath%\SDL2main.lib /ignore:4099
 
 pushd ..\build\
--- code/ray.cpp
+++ code/ray.cpp
@@ -10,6 +10,19 @@
 #include "rivten_math.h"
 #include "random.h"
 
+#define USE_ENTROPY 0
+
+#if !USE_ENTROPY
+#include <time.h>
+#include <stdlib.h>
+internal float
+RandomUnilateral(void)
+{
+	float Result = (float)(rand()) / (float)(RAND_MAX + 1);
+	return(Result);
+}
+#endif
+
 #define SDL_CHECK(Op) {s32 Result = (Op); Assert(Result == 0);}
 #define MAX_FLOAT32 FLT_MAX
 
@@ -17,7 +30,8 @@ global_variable u32 GlobalWindowWidth = 1024;
 global_variable u32 GlobalWindowHeight = 1024;
 global_variable bool GlobalRunning = true;
 global_variable bool GlobalComputed = false;
-global_variable u32 GlobalAACount = 20;
+global_variable u32 GlobalAACount = 20000;
+
 
 internal u32
 RGBToPixel(u8 R, u8 G, u8 B)
@@ -40,6 +54,14 @@ RGBToPixel(v3 Color)
 	return(RGBToPixel(R, G, B));
 }
 
+internal u32
+LinearToPixel(v3 Color)
+{
+	v3 GammaCorrectedColor = V3(SquareRoot(Color.x),
+			SquareRoot(Color.y), SquareRoot(Color.z));
+	return(RGBToPixel(GammaCorrectedColor));
+}
+
 struct camera
 {
 	v3 P;
@@ -51,6 +73,14 @@ struct sphere
 {
 	float Radius;
 	v3 P;
+	u32 MaterialIndex;
+};
+
+struct material
+{
+	v3 Albedo;
+	float Attenuation;
+	float Scatter; // NOTE(hugo): 0 : No scatter (full specular), 1 : full diffuse
 };
 
 struct persistent_render_value
@@ -64,6 +94,9 @@ struct render_state
 {
 	u32 SphereCount;
 	sphere Spheres[256];
+
+	u32 MaterialCount;
+	material Materials[256];
 
 	camera Camera;
 	float FocalLength;
@@ -81,12 +114,26 @@ struct ray
 	v3 Dir;
 };
 
+inline v3
+Reflect(v3 V, v3 N)
+{
+	return(V - 2.0f * Dot(V, N) * N);
+}
+
 internal void
 PushSphere(render_state* RenderState, sphere S)
 {
 	Assert(RenderState->SphereCount < ArrayCount(RenderState->Spheres));
 	RenderState->Spheres[RenderState->SphereCount] = S;
 	++RenderState->SphereCount;
+}
+
+internal void
+PushMaterial(render_state* RenderState, material M)
+{
+	Assert(RenderState->MaterialCount < ArrayCount(RenderState->Materials));
+	RenderState->Materials[RenderState->MaterialCount] = M;
+	++RenderState->MaterialCount;
 }
 
 inline float
@@ -106,10 +153,111 @@ struct hit_record
 	v3 P;
 	v3 N;
 	float t;
+	u32 MaterialIndex;
 };
 
+internal v3
+GetRandomPointInUnitSphere(random_series* Entropy)
+{
+	v3 Result = {};
+	do
+	{
+#if USE_ENTROPY
+		float x = RandomUnilateral(Entropy);
+		float y = RandomUnilateral(Entropy);
+		float z = RandomUnilateral(Entropy);
+#else
+		float x = RandomUnilateral();
+		float y = RandomUnilateral();
+		float z = RandomUnilateral();
+#endif
+		Result = 2.0f * V3(x, y, z) - V3(1.0f, 1.0f, 1.0f);
+	} while(LengthSqr(Result) >= 1.0f);
+	return(Result);
+}
+
+internal v3
+ShootRay(render_state* RenderState, ray Ray)
+{
+	hit_record ClosestHitRecord = {};
+	ClosestHitRecord.t = MAX_FLOAT32;
+
+	for(u32 SphereIndex = 0; SphereIndex < RenderState->SphereCount; ++SphereIndex)
+	{
+		sphere* S = RenderState->Spheres + SphereIndex;
+		float A = LengthSqr(Ray.Dir);
+		float B = 2.0f * Dot(Ray.Start - S->P, Ray.Dir);
+		float C = LengthSqr(Ray.Start - S->P) - S->Radius * S->Radius;
+		// NOTE(hugo): The hit equation then becomes
+		// A * t * t + B * t + C = 0
+		float Delta = B * B - 4.0f * A * C;
+
+		if(Delta < 0)
+		{
+		}
+		else if(Delta == 0.0f)
+		{
+			float t0 = - B / (2.0f * A);
+			if(t0 > 0)
+			{
+				if(t0 < ClosestHitRecord.t)
+				{
+					// NOTE(hugo): We hit something forward
+					ClosestHitRecord.t = t0;
+					ClosestHitRecord.P = Ray.Start + t0 * Ray.Dir;
+					// TODO(hugo): @Optim : we know that the norm is radius ?
+					ClosestHitRecord.N = Normalized(ClosestHitRecord.P - S->P);
+					ClosestHitRecord.MaterialIndex = S->MaterialIndex;
+				}
+			}
+		}
+		else // Delta > 0
+		{
+			// NOTE(hugo): Since A > 0 (it's a norm), we know
+			// that t1 < t2
+			float t1 = (- B - SquareRoot(Delta)) / (2.0f * A);
+			float t2 = (- B + SquareRoot(Delta)) / (2.0f * A);
+
+			if(t1 > 0)
+			{
+				if(t1 < ClosestHitRecord.t)
+				{
+					ClosestHitRecord.t = t1;
+					ClosestHitRecord.P = Ray.Start + t1 * Ray.Dir;
+					// TODO(hugo): @Optim : we know that the norm is radius ?
+					ClosestHitRecord.N = Normalized(ClosestHitRecord.P - S->P);
+					ClosestHitRecord.MaterialIndex = S->MaterialIndex;
+				}
+			}
+			else if(t2 > 0)
+			{
+				// NOTE(hugo): We are inside the sphere ??
+			}
+		}
+
+	}
+	if(ClosestHitRecord.t < MAX_FLOAT32)
+	{
+		ray NextRay = {};
+		NextRay.Start = ClosestHitRecord.P;
+		v3 TargetDiffuse = ClosestHitRecord.N + GetRandomPointInUnitSphere(&RenderState->Entropy);
+		v3 TargetSpecular = Reflect(Ray.Dir, ClosestHitRecord.N);
+		material* M = RenderState->Materials + ClosestHitRecord.MaterialIndex;
+
+		NextRay.Dir = Normalized(Lerp(TargetSpecular, M->Scatter, TargetDiffuse));
+		return(M->Attenuation * Hadamard(M->Albedo, ShootRay(RenderState, NextRay)));
+	}
+	else
+	{
+		// NOTE(hugo): No hit : Background color
+		float t = 0.5f * (1.0f + Ray.Dir.y);
+		return(Lerp(V3(1.0f, 1.0f, 1.0f), t, V3(0.5f, 0.7f, 1.0f)));
+
+	}
+}
+
 internal void
-DrawBackbuffer(render_state* RenderState, u32* Backbuffer)
+RenderBackbuffer(render_state* RenderState, v3* Backbuffer)
 {
 	float ScreenWidth = RenderState->PersistentRenderValue.ScreenWidth;
 	float ScreenHeight = RenderState->PersistentRenderValue.ScreenHeight;
@@ -119,99 +267,36 @@ DrawBackbuffer(render_state* RenderState, u32* Backbuffer)
 	{
 		for(u32 X = 0; X < GlobalWindowWidth; ++X)
 		{
-			u32* Pixel = Backbuffer + X + Y * GlobalWindowWidth;
-			v3 Color = V3(0.0f, 0.0f, 0.0f);
+			v3* Color = Backbuffer + X + Y * GlobalWindowWidth;
 
-			for(u32 AAIndex = 0; AAIndex < GlobalAACount; ++AAIndex)
-			{
-				ray Ray = {};
-				Ray.Start = RenderState->Camera.P;
-				float XOffset = RandomUnilateral(&RenderState->Entropy);
-				float YOffset = RandomUnilateral(&RenderState->Entropy);
-				Assert(XOffset >= 0.0f && XOffset <= 1.0f);
-				Assert(YOffset >= 0.0f && YOffset <= 1.0f);
-				v2 PixelRelativeCoordInScreen = V2(((float(X) + XOffset) / float(GlobalWindowWidth)) - 0.5f, 0.5f - ((float(Y) + YOffset) / float(GlobalWindowHeight)));
-				v3 PixelWorldSpace = RenderState->Camera.P - RenderState->FocalLength * RenderState->Camera.ZAxis + 
-					PixelRelativeCoordInScreen.x * ScreenWidth * RenderState->Camera.XAxis + PixelRelativeCoordInScreen.y * ScreenHeight * CameraYAxis;
-				// TODO(hugo): Do we need to have a normalized direction ? Maybe not...
-				Ray.Dir = Normalized(PixelWorldSpace - Ray.Start);
+			ray Ray = {};
+			Ray.Start = RenderState->Camera.P;
+#if USE_ENTROPY
+			float XOffset = RandomUnilateral(&RenderState->Entropy);
+			float YOffset = RandomUnilateral(&RenderState->Entropy);
+#else
+			float XOffset = RandomUnilateral();
+			float YOffset = RandomUnilateral();
+#endif
+			Assert(XOffset >= 0.0f && XOffset <= 1.0f);
+			Assert(YOffset >= 0.0f && YOffset <= 1.0f);
+			v2 PixelRelativeCoordInScreen = V2(((float(X) + XOffset) / float(GlobalWindowWidth)) - 0.5f, 0.5f - ((float(Y) + YOffset) / float(GlobalWindowHeight)));
+			v3 PixelWorldSpace = RenderState->Camera.P - RenderState->FocalLength * RenderState->Camera.ZAxis + 
+				PixelRelativeCoordInScreen.x * ScreenWidth * RenderState->Camera.XAxis + PixelRelativeCoordInScreen.y * ScreenHeight * CameraYAxis;
+			// TODO(hugo): Do we need to have a normalized direction ? Maybe not...
+			Ray.Dir = Normalized(PixelWorldSpace - Ray.Start);
 
-				hit_record ClosestHitRecord = {};
-				ClosestHitRecord.t = MAX_FLOAT32;
-
-				for(u32 SphereIndex = 0; SphereIndex < RenderState->SphereCount; ++SphereIndex)
-				{
-					sphere* S = RenderState->Spheres + SphereIndex;
-					float A = LengthSqr(Ray.Dir);
-					float B = 2.0f * Dot(Ray.Start - S->P, Ray.Dir);
-					float C = LengthSqr(Ray.Start - S->P) - S->Radius * S->Radius;
-					// NOTE(hugo): The hit equation then becomes
-					// A * t * t + B * t + C = 0
-					float Delta = B * B - 4.0f * A * C;
-
-					if(Delta < 0)
-					{
-					}
-					else if(Delta == 0.0f)
-					{
-						float t0 = - B / (2.0f * A);
-						if(t0 > 0)
-						{
-							if(t0 < ClosestHitRecord.t)
-							{
-								// NOTE(hugo): We hit something forward
-								ClosestHitRecord.t = t0;
-								ClosestHitRecord.P = Ray.Start + t0 * Ray.Dir;
-								// TODO(hugo): @Optim : we know that the norm is radius ?
-								ClosestHitRecord.N = Normalized(ClosestHitRecord.P - S->P);
-							}
-						}
-					}
-					else // Delta > 0
-					{
-						// NOTE(hugo): Since A > 0 (it's a norm), we know
-						// that t1 < t2
-						float t1 = (- B - SquareRoot(Delta)) / (2.0f * A);
-						float t2 = (- B + SquareRoot(Delta)) / (2.0f * A);
-
-						if(t1 > 0)
-						{
-							if(t1 < ClosestHitRecord.t)
-							{
-								ClosestHitRecord.t = t1;
-								ClosestHitRecord.P = Ray.Start + t1 * Ray.Dir;
-								// TODO(hugo): @Optim : we know that the norm is radius ?
-								ClosestHitRecord.N = Normalized(ClosestHitRecord.P - S->P);
-							}
-						}
-						else if(t2 > 0)
-						{
-							// NOTE(hugo): We are inside the sphere ??
-						}
-					}
-
-				}
-				if(ClosestHitRecord.t < MAX_FLOAT32)
-				{
-					Color += 0.5f * (V3(1.0f, 1.0f, 1.0f) + ClosestHitRecord.N);
-				}
-				else
-				{
-					// NOTE(hugo): No hit : Background color
-					float t = 0.5f * (1.0f + Ray.Dir.y);
-					Color += Lerp(V3(1.0f, 1.0f, 1.0f), t, V3(0.5f, 0.7f, 1.0f));
-
-				}
-			}
-
-			Color = Color / float(GlobalAACount);
-			*Pixel = RGBToPixel(Color);
+			*Color += ShootRay(RenderState, Ray);
 		}
 	}
 }
 
 int main(int ArgumentCount, char** Arguments)
 {
+#if !USE_ENTROPY
+	srand(time(NULL));
+#endif
+
 	u32 SDLInitParams = SDL_INIT_EVERYTHING;
 	SDL_CHECK(SDL_Init(SDLInitParams));
 
@@ -228,7 +313,7 @@ int main(int ArgumentCount, char** Arguments)
 	SDL_Surface* Screen = SDL_GetWindowSurface(Window);
 	Assert(Screen);
 
-	u32* Backbuffer = AllocateArray(u32, GlobalWindowWidth * GlobalWindowHeight);
+	v3* Backbuffer = AllocateArray(v3, GlobalWindowWidth * GlobalWindowHeight);
 
 	render_state RenderState = {};
 	RenderState.Camera.P = V3(0.0f, 0.0f, 10.0f);
@@ -244,9 +329,18 @@ int main(int ArgumentCount, char** Arguments)
 
 	RenderState.Entropy = RandomSeed(1234);
 
-	PushSphere(&RenderState, {1.0f, V3(-1.0f, 1.0f, 5.0f)});
-	PushSphere(&RenderState, {2.0f, V3(0.0f, 0.0f, 0.0f)});
-	PushSphere(&RenderState, {150.0f, V3(0.0f, -152.0f, -10.0f)});
+	PushMaterial(&RenderState, {V3(0.8f, 0.2f, 0.1f), 0.5f, 0.9f});
+	PushMaterial(&RenderState, {V3(0.2f, 1.0f, 0.5f), 0.5f, 0.5f});
+	PushMaterial(&RenderState, {V3(0.0f, 0.7f, 1.0f), 0.8f, 0.1f});
+
+	PushSphere(&RenderState, {1.0f, V3(-1.0f, 0.5f, 5.0f), 0});
+	PushSphere(&RenderState, {2.0f, V3(0.0f, 0.0f, 0.0f), 1});
+	PushSphere(&RenderState, {150.0f, V3(0.0f, -152.0f, -10.0f), 2});
+	PushSphere(&RenderState, {0.5f, V3(1.5f, -2.2f, 3.0f), 0});
+	PushSphere(&RenderState, {3.0f, V3(-4.0f, 1.0f, 0.0f), 1});
+	PushSphere(&RenderState, {2.5f, V3(4.0f, 1.0f, 0.0f), 2});
+
+	u32 CurrentAAIndex = 0;
 
 	while(GlobalRunning)
 	{
@@ -268,12 +362,17 @@ int main(int ArgumentCount, char** Arguments)
 		}
 		// }
 
-		if(!GlobalComputed)
+		if(CurrentAAIndex < GlobalAACount)
 		{
-			DrawBackbuffer(&RenderState, Backbuffer);
-			memcpy(Screen->pixels, Backbuffer, sizeof(u32) * GlobalWindowWidth * GlobalWindowHeight);
-			//u32* FirstPixel = (u32*)Screen->pixels;
-			//*FirstPixel = RGBToPixel(V3(1.0f, 0.0f, 0.0f));
+			printf("Rendering pass %i\n", CurrentAAIndex);
+			RenderBackbuffer(&RenderState, Backbuffer);
+			++CurrentAAIndex;
+			for(u32 PixelIndex = 0; PixelIndex < GlobalWindowWidth * GlobalWindowHeight; ++PixelIndex)
+			{
+				u32* Pixel = (u32*)(Screen->pixels) + PixelIndex;
+				v3 Color = Backbuffer[PixelIndex];
+				*Pixel = LinearToPixel(Color / float(CurrentAAIndex));
+			}
 			SDL_UpdateWindowSurface(Window);
 			GlobalComputed = true;
 		}
